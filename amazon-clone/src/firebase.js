e83a4e405e20d45339192d245424be32cea184a4
Shourya <shourya1017.cse18@chitkara.edu.in>
2020-09-15 22:49:25 +0530

    initial commit header done

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7kFwUDa_ag5TBhz0UaYJSpLSXZxMlNt0",
+  authDomain: "clone-10526.firebaseapp.com",
+  databaseURL: "https://clone-10526.firebaseio.com",
+  projectId: "clone-10526",
+  storageBucket: "clone-10526.appspot.com",
+  messagingSenderId: "278524873644",
+  appId: "1:278524873644:web:a0ca9086ff2cf6a3f88809",
+  measurementId: "G-EKHGJ3ESXJ",
+};
