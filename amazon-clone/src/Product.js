d24ce579c4b8d6e09439cc32c591b090f37f0c32
Shourya <shourya1017.cse18@chitkara.edu.in>
2020-09-18 21:16:32 +0530

    Front Page and Checkout Page Completed

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,22 +1,42 @@
 import React from "react";
 import "./Product.css";
-function Product() {
+import { useStateValue } from "./StateProvider";
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //put items into data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
-        <p>Kohli StartUp</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>Rs</small>
-          <strong>750</strong>
+          <strong>{price}</strong>
         </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
       </div>
 
-      <div className="product__rating">
-        <p>⭐</p>
-      </div>
-
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR90uO2BtTD1j_ktPAT86Q6wLDYWwzrBHsyZeLF5gyxPJ3kM0WYXtFZV-UKuFEt-DowMBFhkVBZ&usqp=CAc" />
-      <button>Add To Basket</button>
+      <img src={image} />
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
